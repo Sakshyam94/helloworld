38e75ab3f4c47d49d98150ca93a2d50787a15855
Sakshyam Timilsina <stimilsina3@myseneca.ca>
2023-05-12 11:14:36 -0400

    final commit

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
+/**
+* WEB322 - Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic 
+Policy. 
+* No part of this assignment has been copied manually or electronically from any other 
+source
+* (including web sites and friends) or distributed to other students.
+* I understand that if caught doing so, I will receive zero on this assignment and 
+possible 
+* fail the entire course.
+*
+* Name: Sakshyam Timislina
+* Student ID: 155496219
+* Date: 12th May 2023
+* Cyclic Web App URL:https://app.cyclic.sh/#/app/sakshyam94-helloworld/overview
+* GitHub Repository URL:https://github.com/Sakshyam94/helloworld/tree/master
+*
+*/
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
